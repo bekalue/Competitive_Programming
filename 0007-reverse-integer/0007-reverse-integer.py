b0ce868fbb5074dbaaff5b2c2c2ddf0d8b90595d
Bekalu Geta <79188583+bekalue@users.noreply.github.com>
2023-07-02 18:35:36 +0300

    Time: 39 ms (95.34%), Space: 16.4 MB (29.06%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,21 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        MAX_INT = 2**31 - 1
-        MIN_INT = -2**31
         is_negative = x < 0
         x = abs(x)
         result = 0
     
         while x:
-            digit = x % 10
-            x //= 10
+            x, last_digit = divmod(x, 10)
+            result = result * 10 + last_digit
     
-            if result > MAX_INT // 10 or (result == MAX_INT // 10 and digit > 7):
+            if result < -2**31 or result > 2**31 - 1:
                 return 0
-            if result < MIN_INT // 10 or (result == MIN_INT // 10 and digit < -8):
-                return 0
-    
-            result = result * 10 + digit
     
         return -result if is_negative else result
-        
+    
